ae513fb30fbc3aa853d8cb0f631c1c4579300f2d
Baylor Rae <baylor@thecodedeli.com>
2018-02-03 08:32:07 -0600

    add hangfire dashboard with redis

---
+++ src/frontend/Lib/DashboardAuthorizationFilter.cs
@@ -0,0 +1,9 @@
+﻿using Hangfire.Dashboard;
+
+namespace frontend.Lib
+{
+    public class DashboardAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize(DashboardContext context) => true;
+    }
+}
--- src/frontend/Startup.cs
+++ src/frontend/Startup.cs
@@ -2,18 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using frontend.Lib;
+using Hangfire;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using StackExchange.Redis;
 
 namespace frontend
 {
     public class Startup
     {
+        private static ConnectionMultiplexer RedisConnection { get; set; }
+        
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            RedisConnection = ConnectionMultiplexer.Connect(Configuration.GetConnectionString("redis"));
         }
 
         public IConfiguration Configuration { get; }
@@ -22,6 +29,11 @@ namespace frontend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddHangfire(options =>
+            {
+                options.UseRedisStorage(RedisConnection);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +49,11 @@ namespace frontend
             }
 
             app.UseStaticFiles();
+
+            app.UseHangfireDashboard("/hangfire", new DashboardOptions
+            {
+                Authorization = new[] {new DashboardAuthorizationFilter()}
+            });
 
             app.UseMvc(routes =>
             {
