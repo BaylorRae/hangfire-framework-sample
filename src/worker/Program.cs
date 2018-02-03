428ed67cecec6119d41865f342b22fb261e659ec
Baylor Rae <baylor@thecodedeli.com>
2018-02-03 08:58:14 -0600

    add worker

---
+++ src/worker/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Hangfire;
+using StackExchange.Redis;
+
+namespace worker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var redis = ConnectionMultiplexer.Connect("redis");
+
+            GlobalConfiguration.Configuration.UseRedisStorage(redis);
+
+            using (var server = new BackgroundJobServer())
+            {
+                Console.WriteLine("Hangfire Server started. Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
